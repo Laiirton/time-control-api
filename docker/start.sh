d7094eaba9fea95f471171368aa4ce0c2accf20b
Lairton <130435095+Laiirton@users.noreply.github.com>
2026-03-04 17:29:24 -0300

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -15,6 +15,13 @@ envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/con
 
 cd /var/www
 
+if [ "${DB_FORCE_IPV4:-true}" = "true" ] && [ -n "$DB_HOST" ]; then
+    DB_HOST_IPV4=$(php -r '$host = getenv("DB_HOST"); if (!$host) { exit(0); } $ipv4 = gethostbyname($host); if ($ipv4 !== $host) { echo $ipv4; }')
+    if [ -n "$DB_HOST_IPV4" ]; then
+        export DB_HOST="$DB_HOST_IPV4"
+    fi
+fi
+
 php artisan config:cache
 php artisan view:cache
 php artisan migrate --force

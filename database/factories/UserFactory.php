59589fa646c2173a7f66b0e59a6cd2e7d6f4142f
Lairton <130435095+Laiirton@users.noreply.github.com>
2026-03-04 04:03:28 -0300

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,6 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -25,20 +24,14 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'type' => fake()->word(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'role' => fake()->jobTitle(),
+            'department' => fake()->word(),
+            'phone' => fake()->phoneNumber(),
+            'location' => fake()->city(),
+            'shift' => fake()->randomElement(['morning', 'afternoon', 'night']),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
